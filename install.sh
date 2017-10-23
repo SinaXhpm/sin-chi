4c1fd2652b0b4bec51f01d8f6592712a1f612f33
Sina Xhpm <harvast68@gmail.com>
2017-10-23 21:06:02 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-cd $HOME/SinChi
+cd $HOME/sin-chi
 wget https://valtman.name/files/telegram-bot-171022-nightly-linux
 mv telegram-bot-171022-nightly-linux tb
+
  sudo apt-get update -y 
  sudo apt-get upgrade -y
  sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev -y
@@ -25,7 +26,12 @@ wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
  sudo luarocks install dkjson
  sudo luarocks install lanes
  sudo luarocks install Lua-cURL
+ sudo apt-get install libconfig++8-dev -y
 cd ..
  echo -n "" > sudo.lua
   read -p "What is your User Id? " -e input
 echo "sudo={111795059,$input}" >> sudo.lua
+chmod +x create.sh
+chmod +x tb
+chmod +x on.sh
+./tb
