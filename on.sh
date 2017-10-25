018f706d7945dd7f2a297b52917e27e384457cbc
Sina Xhpm <harvast68@gmail.com>
2017-10-25 18:22:24 +0330

    Update on.sh

--- on.sh
+++ on.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 while true ; do
    killall tmux
+   killall tb
 for entr in sinchi-*.sh ; do
     entry="${entr/.sh/}"
     tmux new-session -d -s $entry "./tb -p "$entry" -s bot.lua"
   done
   echo 'On!'
-sleep 1080
+sleep 250
 done
